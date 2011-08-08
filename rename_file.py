a66e8b84be438c3a4494797474e5ced40f8a93d0
Tiny <plakitboy@gmail.com>
2011-08-08 21:46:12 +0800

    first commit

---
+++ rename_file.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Filename:  del_pyc.py
+# Author: iLazy
+# Date:2011-03-26
+
+import os
+import fnmatch
+def del_pyc(root, patterns='*'):
+        """del all the pyc file in the current work space
+        """
+        patterns = patterns.split(';')
+        for path, subdir, files in os.walk(root):
+                for name in files:
+                        for pattern in patterns:
+                               if fnmatch.fnmatch(name, pattern.strip()):
+                                        yield os.path.join(path, name)
+
+if __name__=='__main__':
+        for path in del_pyc(os.getcwd(),'*.py'):
+                #print path
+                #os.remove(path)
+		dirname = os.path.dirname(path)
+		basename = os.path.basename(path)
+		root, extention = os.path.splitext(basename)
+		rename = os.path.basename(os.path.splitext(path)[0] +'.txt')
+		os.rename(path,dirname + os.sep + rename)
+                #print path
+                #print (dirname+rename)
+
