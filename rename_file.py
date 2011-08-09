07652109b46d3df2acbcb6809031a6b70f5da4f0
Tiny <plakitboy@gmail.com>
2011-08-09 17:33:43 +0800

    modify format

--- rename_file.py
+++ rename_file.py
@@ -7,24 +7,20 @@
 import os
 import fnmatch
 def del_pyc(root, patterns='*'):
-        """del all the pyc file in the current work space
-        """
-        patterns = patterns.split(';')
-        for path, subdir, files in os.walk(root):
-                for name in files:
-                        for pattern in patterns:
-                               if fnmatch.fnmatch(name, pattern.strip()):
-                                        yield os.path.join(path, name)
+    """
+    del all the pyc file in the current work space
+     """
+    patterns = patterns.split(';')
+    for path, subdir, files in os.walk(root):
+        for name in files:
+            for pattern in patterns:
+                if fnmatch.fnmatch(name, pattern.strip()):
+                    yield os.path.join(path, name)
 
 if __name__=='__main__':
-        for path in del_pyc(os.getcwd(),'*.py'):
-                #print path
-                #os.remove(path)
-		dirname = os.path.dirname(path)
-		basename = os.path.basename(path)
-		root, extention = os.path.splitext(basename)
-		rename = os.path.basename(os.path.splitext(path)[0] +'.txt')
-		os.rename(path,dirname + os.sep + rename)
-                #print path
-                #print (dirname+rename)
-
+    for path in del_pyc(os.getcwd(),'*.py'):
+        dirname = os.path.dirname(path)
+        basename = os.path.basename(path)
+        root, extention = os.path.splitext(basename)
+        rename = os.path.basename(os.path.splitext(path)[0] +'.txt')
+        os.rename(path,dirname + os.sep + rename)
